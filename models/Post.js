9b42567c6f3614392c810bd4a9ce8a82ca2f8578
Cloudbop <boppincolin@gmail.com>
2019-10-28 01:37:55 +0000

    Implemented Post add Comment Route. Created Comment model. Which refs to user and post.  Updated Post model with array of comment ids.  Prettied models.

--- models/Post.js
+++ models/Post.js
@@ -2,61 +2,62 @@ const mongoose = require('mongoose');
 // const URLSlugs = require('mongoose-url-slugs');
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'users'
-  },
-
-  category: {
-    type: Schema.Types.ObjectId,
-    ref: 'categories'
-  },
-
-  title: {
-    type: String,
-    required: true
-  },
-
-  // slug: {
-
-  //     type: String
-
-  // },
-
-  status: {
-    type: String,
-    default: 'public'
-  },
-
-  allowComments: {
-    type: Boolean,
-    require: true
-  },
-
-  body: {
-    type: String,
-    require: true
-  },
-
-  file: {
-    type: String
-  },
-  // ids of all the comments
-  comments: [
-    {
+const PostSchema = new Schema(
+  {
+    user: {
       type: Schema.Types.ObjectId,
-      ref: 'comments'
+      ref: 'users'
+    },
+
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'categories'
+    },
+
+    title: {
+      type: String,
+      required: true
+    },
+
+    // slug: {
+
+    //     type: String
+
+    // },
+
+    status: {
+      type: String,
+      default: 'public'
+    },
+
+    allowComments: {
+      type: Boolean,
+      require: true
+    },
+
+    body: {
+      type: String,
+      require: true
+    },
+
+    file: {
+      type: String
+    },
+    // ids of all the comments
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'comments'
+      }
+    ],
+
+    date: {
+      type: Date,
+      default: Date.now()
     }
-  ],
-
-  date: {
-    type: Date,
-    default: Date.now()
-  }
-});
-
-// }) //, {usePushEach: true});
+  },
+  { usePushEach: true }
+);
 
 // PostSchema.plugin(URLSlugs('title', {field: 'slug'}));
 
--- routes/admin/comments.js
+++ routes/admin/comments.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const router = express.Router();
+// models
+const Post = require('../../models/Post');
+const Comment = require('../../models/Comment');
 
+// FORM[HIDDEN]=POST AT http://localhost:4500/admin/comments
 router.post('/', (req, res) => {
-  res.send('it work');
+  // pass post ID (for mongoose Post.model)
+  Post.findOne({ _id: req.body.id }).then(post => {
+    // create new comment
+    const newComment = new Comment({
+      // req.user stored as cookie on login
+      user: req.user.id,
+      body: req.body.body
+    });
+    // console.log('newComment', newComment);
+    // store newComment
+    post.comments.push(newComment);
+    // save to post
+    post.save().then(savedPost => {
+      // save to comment
+      // console.log('savedPost', savedPost);
+      newComment.save().then(savedComment => {
+        // console.log('savedComment', savedComment);
+        res.redirect(`/posts/${post.id}`);
+      });
+    });
+  });
+
+  // res.send('it work');
 });
 
 module.exports = router;
+/**
+ * 
+ * Remember that req.user is created by session @ login. Passport taking care of this.
+ */
